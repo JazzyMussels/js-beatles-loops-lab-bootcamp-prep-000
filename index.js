dd7fad7aee60f19a0556c0892e4f035d477d54bf
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-09 21:00:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var new_array = []
   for (var i = 0; i < musicians.length - 1; i += 1) {
     new_array.push(`${musicians[i]} plays ${instruments[i]}`)
+    i+=1
   }
   return new_array
 }
