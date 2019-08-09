a04fb049df3b98416c4a400c7d341c347e7526e1
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-09 21:06:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var i = 0
   while (i < facts.length) {
-    facts = facts[i] + '!!!'
+    facts = (facts[i] + '!!!')
     i += 1
   }
   return facts
